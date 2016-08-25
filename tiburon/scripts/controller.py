1233339d85baf60bb8c7c78086b392977db3f4cd
Rohit Suri <rohitsuri.nitr@gmail.com>
2016-08-25 20:46:43 +0530

    vehicle params added

--- tiburon/scripts/controller.py
+++ tiburon/scripts/controller.py
@@ -6,6 +6,7 @@ from dynamic_reconfigure.server import Server
 from tiburon.cfg import depthparamsConfig
 import datetime,time
 from PID import PID
+from vehicle import VehicleParams
 
 depthController = PID()
 pitchController = PID()
@@ -45,7 +46,28 @@ def main():
         yawU = yawController.getError()
         forwardU = 0
 
-        
+        '''
+        Bottom w.r.t. to bot is positive z
+        Forward w.r.t. to bot is positive y
+        Right wrt to forward direction of bot is positive x
+
+        F1 - forward, F2 - backward, F3 - left, F4 - right
+
+        Equations of motion are:-
+        F1 cos θ + F2 cos θ + F3 sin θ + F4 sin θ = ma + B - mg = U1 + B - mg
+        - F1 sin θ - F2 sin θ + F3 cos θ + F4 cos θ = 0 = U2
+        - F1 x1 + F2 x2 = Ixx αxx = U3
+        F3 x3 - F4 x4 = Izz αzz = U4
+
+        Matrix form
+        #  ==                                  ==   ==    ==         ==            ==
+        |   cos θ    cos θ    sin θ    sin θ   | |   F1   |       |   U1 + B - mg  |
+        |  -sin θ   -sin θ    cos θ    cos θ   | |   F2   |  ===  |       00       |
+        |   -x1       x2       00       00     | |   F3   |  ===  |       U3       |
+        |    00       00       x3      -x4     | |   F4   |       |       U4       |
+         ==                                  ==   ==    ==         ==            ==
+
+        '''
 
 
 
---
+++ tiburon/scripts/vehicle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+class VehicleParams():
+    def __init__(self):
+        self.B = 0.00 # Buoyancy force
+        self.mass = 0.00
+        self.gravity = 9.81
+        # 1- forward. 2-backward, 3-left, 4-right
+        self.x1 = 0.00
+        self.x2 = 0.00
+        self.x3 = 0.00
+        self.x4 = 0.00

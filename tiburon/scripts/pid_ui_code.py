094c2bd3a77a4b1fa33ce20d608c42dcb014ace5
sonali9696 <sonali9696@gmail.com>
2016-09-04 19:23:56 +0530

    pid_ui_code updated

--- tiburon/scripts/pid_ui_code.py
+++ tiburon/scripts/pid_ui_code.py
@@ -36,40 +36,75 @@ class pidPanel(QtGui.QMainWindow):
         self.pubsNsubs()
         self.graph_depth()
         self.graph_pitch()
-    
+        
+        self.kp_yaw=0.00
+        self.ki_yaw=0.00
+        self.kd_yaw=0.00
+        self.ckpoint_yaw=0.00
+        
+        self.kp_pitch=0.00
+        self.ki_pitch=0.00
+        self.kd_pitch=0.00
+        self.ckpoint_pitch=0.00
+        
+        self.kp_depth=0.00
+        self.ki_depth=0.00
+        self.kd_depth=0.00
+        self.ckpoint_depth=0.00
+        
+        self._timer = QtCore.QTimer(self)
+        self._timer.timeout.connect(self.update)
+        self._timer.start(10)
+
+    def update(self):
+        global setpoint_depth
+        global setpoint_pitch
+
+        self.ui.ckpoint_lineEdit.setText(str(self.ckpoint_yaw))
+        self.ui.kp_yaw_lineEdit.setText(str(self.kp_yaw)) #kp
+        self.ui.ki_yaw_lineEdit.setText(str(self.ki_yaw)) #ki
+        self.ui.kd_yaw_lineEdit.setText(str(self.kd_yaw)) #kd
+
+        self.ui.kp_depth_lineEdit.setText(str(self.kp_depth)) #kp
+        self.ui.ki_depth_lineEdit.setText(str(self.ki_depth)) #ki
+        self.ui.kd_depth_lineEdit.setText(str(self.kd_depth)) #kd
+        self.ui.setpoint_depth_lineEdit.setText(str(setpoint_depth)) 
+
+        self.ui.kp_pitch_lineEditlineEdi.setText(str(kp_pitch)) #kp
+        self.ui.ki_pitch_lineEdit.setText(str(ki_pitch)) #ki
+        self.ui.kd_pitch_lineEdit.setText(str(kd_pitch)) #kd
+        self.ui.setpoint_pitch_lineEdit.setText(str(setpoint_pitch))
+
     def yawCallback(self,msg):
-        self.ui.ckpoint_lineEdit.setText(str(msg.YPR.x)) 
+        self.ckpoint_yaw=msg.YPR.x
         
     #doubles is an array whose contents can be printed by msg.doubles
     #The required kp,ki,kd value of yaw is stored in the given positions
 
     def srv_y_callback(self,msg):
         #print msg.doubles
-        self.ui.kp_yaw_lineEdit.setText(str(msg.doubles[3].value)) #kp
-        self.ui.ki_yaw_lineEdit.setText(str(msg.doubles[4].value)) #ki
-        self.ui.kd_yaw_lineEdit.setText(str(msg.doubles[7].value)) #kd
-    
+        self.kp_yaw = msg.doubles[3].value
+        self.ki_yaw = msg.doubles[4].value
+        self.kd_yaw = msg.doubles[7].value
+        
     #def depthCallback(self,msg):
     #    self.ui.ckpoint_depth_lineEdit.setText(str(msg.data)) 
     
     def srv_d_callback(self,msg):
         global setpoint_depth
-        self.ui.kp_depth_lineEdit.setText(str(msg.doubles[3].value)) #kp
-        self.ui.ki_depth_lineEdit.setText(str(msg.doubles[0].value)) #ki
-        self.ui.kd_depth_lineEdit.setText(str(msg.doubles[2].value)) #kd
+        self.kp_depth = msg.doubles[3].value
+        self.ki_depth = msg.doubles[0].value
+        self.kd_depth = msg.doubles[2].value
         setpoint_depth = msg.doubles[1].value
-        self.ui.setpoint_depth_lineEdit.setText(str(setpoint_depth)) 
-    
+        
     def srv_p_callback(self,msg):
         #print msg.doubles
         global setpoint_pitch
-        self.ui.kp_pitch_lineEditlineEdi.setText(str(msg.doubles[1].value)) #kp
-        self.ui.ki_pitch_lineEdit.setText(str(msg.doubles[3].value)) #ki
-        self.ui.kd_pitch_lineEdit.setText(str(msg.doubles[2].value)) #kd
+        self.kp_pitch = msg.doubles[1].value
+        self.ki_pitch = msg.doubles[3].value
+        self.kd_pitch = msg.doubles[2].value
         setpoint_pitch = msg.doubles[4].value
-        print setpoint_pitch
-        self.ui.setpoint_pitch_lineEdit.setText(str(setpoint_pitch))
-    
+        
     def pubsNsubs(self):
         self.yaw = rospy.Subscriber("/tiburon/ins_data",ins_data,self.yawCallback) 
         self.yaw_srv = rospy.Subscriber("/server_yaw/parameter_updates",Config,self.srv_y_callback)

e76a8c86af16bdcb474c332197669617e724afc7
surirohit <rohitsuri.nitr@gmail.com>
2016-10-31 20:23:22 +0530

    Mode changed

--- tiburon/scripts/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-alias connectauv='ssh auv-nitr@192.168.61.154'
-source /home/sazzy_techgirl/Documents/tiburon-master/devel/setup.bash
-roscd #devel folder
-cd .. #Now we are inside tiburon-master package
-catkin_make #optional
-rosrun tiburon 

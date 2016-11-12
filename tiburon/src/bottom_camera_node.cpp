999df31f942889e6a389e7842c76e0a69f112c6a
surirohit <rohitsuri.nitr@gmail.com>
2016-11-12 11:20:24 +0530

    vision stack

--- tiburon/src/bottom_camera_node.cpp
+++ tiburon/src/bottom_camera_node.cpp
@@ -1,26 +1,228 @@
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <std_msgs/Float64.h>
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <geometry_msgs/Vector3.h>
 #include <tiburon/ins_data.h>
 
 using namespace std;
 using namespace cv;
 
-double yaw = 0.0;
+int findTargetPoint(Mat,Point&);
+double cc_angle(Point vtx,Point p1,Point p12);
+Point findCenter(vector<Point> contour);
+double abs_distance(Point2i p1,Point2i p2);
+int findLargestContour(vector<vector<Point> > contours);
+double cc_angle(Point2i vtx,Point2i p2);
+double getTargetYaw(vector<Point> contour);
+double globalYawDecision(double yaw1,double yaw2);
+void segmentedImageCallback(const sensor_msgs::ImageConstPtr& msg);
+//void origImageCallback(const sensor_msgs::ImageConstPtr& msg);
+void insCallback(const tiburon::ins_data::ConstPtr& ypr);
 
-double abs_distance(Point2i p1,Point2i p2)
+
+#define USELESS -1
+#define TOOCLOSE -10
+#define MOVETOCENTER 10
+#define TOPCROP 0.6 // To go forward - we should only consider going forward
+#define OUTERCIRCLE 0.36
+#define OUTER 100
+#define INNERCIRCLE 0.2 // Radius = cols*innercircle
+#define INNER 50
+
+#define LINENOTFOUND -1
+#define LINETOOFAR 1
+#define DISTANCEPID 2
+#define ANGLEPID 3
+
+double globalYaw = 133.0;
+double currentYaw = 0.0; // #Todo Remove this
+
+Mat orig,disp;
+Mat origGr;
+vector<Point> largestContour;
+
+bool origImageReceived = false, segmentedImageReceived = false;
+
+// #Notes - Three outputs are possible - Angle PID - Will have 2 subparts - Small and Big angle
+//                                      Distance PID - Line is between outer and inner circle
+//                                      Move to Line Center - Line is out of outer circle
+
+
+// #Notes Return Values - Set Yaw, Set Speed, Flag
+// flag states - line not found , move to center(set yaw open loop, go little front open loop), forward distance pid,
+
+// TODO: insCallback, segmentedImageCallback, origImageCallback
+
+int main(int argc, char **argv)
 {
-    //Calculates euclidian distance
-    double sqterm = pow(p1.x-p2.x,2.0)+pow(p1.y-p2.y,2.0);
-    double dist = pow(sqterm,0.5);
-    return dist;
-}
+    ros::init(argc, argv, "BottomCameraNode");
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber segmentedImageSub = it.subscribe("/bottomCameraSegmented",1,segmentedImageCallback);
+    //image_transport::Subscriber origImageSub = it.subscribe("/auv_cam1",1,origImageCallback);
+    image_transport::Publisher dispPub = it.advertise("/debugImage", 1);
+    ros::Subscriber ins = nh.subscribe("/tiburon/ins_data",1,insCallback);
+    ros::Publisher yawPub = nh.advertise<std_msgs::Float64>("/yawSP",1);
+    ros::Publisher velPub = nh.advertise<std_msgs::Float64>("/velSP",1);
+    sensor_msgs::ImagePtr msg;
+    std_msgs::Float64 f64msg;
+    while(1)
+    {
+        //cout<<"Enter"<<endl;
+        ros::spinOnce();
+        if(!segmentedImageReceived)
+            continue;
+        cout<<"NC"<<endl;
+        imshow("gr",origGr.clone());
+        Point botcenter = Point(origGr.cols/2,origGr.rows/2);
+        Size imsize = origGr.size();
+        //waitKey(0);
+        double imdiag = abs_distance(Point2i(0,0),Point2i(origGr.cols,origGr.rows));
+        disp = Mat::zeros(imsize, CV_8UC3);
+        char arr[50];
+        //sprintf(arr,"/home/codestation/garbage/%d.bmp",rand()%20);
+        //cout<<arr<<endl;
+        //imwrite(arr,origGr);
+        Point target = Point(1,0);
+        double yawTarget=globalYaw,speedTarget=0.0,speednormalized = 0.0;
+        bool statusflag = LINENOTFOUND;
+        int targetStatus =  findTargetPoint(origGr,target);
 
-double cc_angle(Point2i vtx,Point2i p1,Point2i p2)
+        if(targetStatus == MOVETOCENTER)
+        {
+            // Set yaw, speed = 0, open loop =1, line not found flag = 0
+            yawTarget = cc_angle(botcenter,target)*180.0/3.1415; // the angle between the camera center and the target
+            speedTarget = abs_distance(botcenter,target);
+            statusflag = LINETOOFAR;
+            circle(disp,target,30,Scalar(255,255,0),-1);
+        }
+        else if(targetStatus == USELESS)
+        {
+            // set line not found =1
+            speedTarget = 0.0;
+            yawTarget = 90;
+            statusflag = LINENOTFOUND;
+        }
+        else if(targetStatus == OUTER)
+        {
+            // TODO: why y component
+            // Set yaw=currentyaw, speed = y component of distance, open loop =0, line not found flag = 0
+            yawTarget = 90;
+            speedTarget = abs(target.y-botcenter.y);
+            statusflag = DISTANCEPID;
+        }
+        else if(targetStatus == INNER)
+        {
+            // Set yaw=currentyaw, speed = y component of distance, open loop =0, line not found flag = 0
+            yawTarget = getTargetYaw(largestContour);
+            cout << "yawtarget" <<  yawTarget << endl;
+            // #Todo if yaw is out of limit ->  speed = 0 else speed = f(|yaw|)*basespeed
+            statusflag = ANGLEPID;
+        }
+        speednormalized = speedTarget/imdiag;
+        // TODO: are the next few lines okay?
+        // All the angles are with respect to the x axis has to be converted to y axis
+        double yawYaxis = 90 - yawTarget;
+        // Set the final yaw angle - final yaw will be atan2 of currentyaw+yawTarget
+        double setFinalYaw = currentYaw+yawYaxis;
+        setFinalYaw = atan2(sin(setFinalYaw*3.1415/180.0),cos(setFinalYaw*3.1415/180.0))*180.0/3.1415;
+        //# TODO - set the speed - which will be some factor of speednormalized
+        // Publish values
+        if(targetStatus == MOVETOCENTER)
+        {
+            if(abs(currentYaw-setFinalYaw)>2.5) // TODO what should be angle?
+            {
+                f64msg.data= 0;
+                velPub.publish(f64msg);
+                f64msg.data = setFinalYaw;
+                yawPub.publish(f64msg);
+                // ros::spinOnce();
+            }
+            // TODO: what to do about forward?
+        }
+        else if(targetStatus == USELESS)
+        {
+            f64msg.data = 0.0;
+            velPub.publish(f64msg);
+            ros::spinOnce();
+        }
+        else if(targetStatus == OUTER)
+        {
+            f64msg.data = speednormalized*4.0;
+            velPub.publish(f64msg); // TODO: should be more I guess
+            ros::spinOnce();
+        }
+        else if(targetStatus == INNER)
+        {
+            if(abs(currentYaw-setFinalYaw)>1.0) // TODO what should be angle?
+            {
+                f64msg.data= 0;
+                velPub.publish(f64msg);
+                f64msg.data = setFinalYaw;
+                yawPub.publish(f64msg);
+                //ros::spinOnce();
+            }
+            // TODO: Forward till the end of time or next POI, whichever is nearer
+        }
+        cout << "Yaw " << setFinalYaw << endl;
+        cout << "Speed " << speednormalized << endl;
+        cout << "Flag " << statusflag << endl;
+
+        msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", disp).toImageMsg();
+        dispPub.publish(msg);
+        //imshow("aa",disp);
+        waitKey(10);
+
+    }
+    return 0;
+}
+int findTargetPoint(Mat bottomCameraImage,Point &targetPoint) //Pass segmented and noise filtered
+{
+    cout<<"Enter1"<<endl;
+    vector<vector<Point> > contours;
+    Mat topCrop = bottomCameraImage(Rect(0,0,bottomCameraImage.cols,bottomCameraImage.rows*TOPCROP)); // Crop the top part , TOPCROP % from top corner
+    cout<<"size"<<topCrop.size()<<endl;
+    findContours(topCrop.clone(),contours,CV_RETR_TREE,CV_CHAIN_APPROX_NONE);
+    //waitKey(0);
+    int largest = findLargestContour(contours);
+    if(largest == -1)
+    {
+        cout << "No line found" << endl;
+
+        return USELESS;
+    }
+    drawContours(disp,contours,largest,Scalar(0,0,255),3);
+    circle(disp,Point(bottomCameraImage.cols/2,bottomCameraImage.rows/2),7,Scalar(255,255,0),-1);
+    Point centerOfLine = findCenter(contours[largest]);
+    largestContour = contours[largest];
+    circle(disp,centerOfLine,3,Scalar(0,255,0),-1);
+    double botToLineDistance = abs_distance(centerOfLine,Point(bottomCameraImage.cols/2,bottomCameraImage.rows/2));
+    if(botToLineDistance > bottomCameraImage.cols*OUTERCIRCLE)
+    {
+
+       targetPoint = centerOfLine;
+       cout << "Move to center";
+       return MOVETOCENTER;
+    }
+    else if(botToLineDistance > bottomCameraImage.cols*INNERCIRCLE)
+    {
+        cout << "Apply distance PID " << endl;
+        return OUTER;
+   }
+   else
+   {
+        cout << "Apply angle PID "  << endl;
+        return INNER;
+   }
+   cout<<"Exit1"<<endl;
+}
+double cc_angle(Point vtx,Point p1,Point p2)
 {
 // 1st point is the vertex , then 1st point and 2nd point,angle is obtained in radians
     double modv1 = abs_distance(vtx,p1);		// Mod v1
@@ -41,206 +243,133 @@ double cc_angle(Point2i vtx,Point2i p1,Point2i p2)
     else
         theta = acos(costheta);
     return theta*sign;
-}
 
+}
+Point findCenter(vector<Point> contour)
+{
+    int count  = 0;
+    Point2f center = Point2f(0,0);
+    for(int i = 0;i < contour.size();i++)
+    {
+        center.x = (center.x*count+contour[i].x)/(count+1);
+        center.y = (center.y*count+contour[i].y)/(count+1);
+        count++;
+    }
+    return center;
+}
+double abs_distance(Point2i p1,Point2i p2)
+{
+    //Calculates euclidian distance
+    double sqterm = pow(p1.x-p2.x,2.0)+pow(p1.y-p2.y,2.0);
+    double dist = pow(sqterm,0.5);
+    return dist;
+}
+int findLargestContour(vector<vector<Point> > contours)
+{
+    cout<<"Enter2"<<endl;
+    int areaMax = 0;//segm.rows*segm.cols;
+    int maxindex = -1;
+    for(int j = 0;j<contours.size();j++)
+    {
+        if(areaMax < contourArea(contours[j]))
+        {
+            maxindex = j;
+            areaMax = contourArea(contours[j]);
+        }
+    }
+    cout<<"Exit2"<<endl;
+    return maxindex;
+}
 double cc_angle(Point2i vtx,Point2i p2)
 {
     Point p1 = Point((vtx.x+30),vtx.y);
     return cc_angle(vtx,p1,p2);
 }
-
-void yaw_callback(const tiburon::ins_data::ConstPtr& ypr_val)
+double getTargetYaw(vector<Point> contour)
 {
-    yaw = ypr_val->YPR.x;
+    cout<<"Enter3"<<endl;
+    double targetyaw;
+    vector<Point> approxContour;
+    approxPolyDP(contour,approxContour,10,true); // works wonders but this epsilon how much to set? - 10 works wonders
+    vector<double> angles;
+    int iter = 0;
+    while(approxContour.size()!=4 && iter <=10)
+    {
+        iter++;
+        approxPolyDP(contour,approxContour,10+iter*2,true);
+    }
+    if(approxContour.size()!=4)
+        return USELESS; // have to check in the function call
+    double diffangle;
+    for(int i= 0;i<approxContour.size();i++)
+    {
+       if(i==approxContour.size()-1)
+            diffangle = cc_angle(approxContour[i],approxContour[0]);
+       else
+            diffangle = cc_angle(approxContour[i],approxContour[i+1]);
+       angles.push_back(diffangle*180/3.1415);
+       cout << "a " << angles[i] << endl;
+    }
+    double angleset1 = abs(angles[0]-angles[2]),angleset2 = abs(angles[1]-angles[3]);
+    if(abs(angleset1-180.0)<15 && abs(angleset2-180.0)<15)
+    {
+     // use the closer angle to the globalyaw
+       double targetyaw1 = globalYawDecision(angles[0],angles[2]);
+       double targetyaw2 = globalYawDecision(angles[1],angles[3]);
+       targetyaw = globalYawDecision(targetyaw1,targetyaw2);
+//       cout << targetyaw1 << " " << targetyaw2 << " decision made " << targetyaw <<endl;
+    }
+    else
+    {
+        targetyaw=(abs(angleset1-180)<abs(angleset2-180))?((angles[0]+180.0-angles[2])/2):((angles[1]+180.0-angles[2])/2);
+        targetyaw=globalYawDecision(targetyaw,180-targetyaw);
+     // use the closer angle to the globalyaw
+    }
+    // approxpolydp will always give points in circular pattern so angles will be separated by 2 if sides = 4
+    cout<<"Exit3"<<endl;
+   return targetyaw;
+}
+double globalYawDecision(double yaw1,double yaw2) // angles should be in degrees
+{
+    cout<<"Enter4"<<endl;
+    double dy1 = yaw1 - globalYaw;
+    double dy2 = yaw2 - globalYaw;
+    dy1 = atan2(sin(3.1415/180.0*dy1),cos(3.1415/180.0*dy1))*180.0/3.1415;
+    dy2 = atan2(sin(3.1415/180.0*dy2),cos(3.1415/180.0*dy2))*180.0/3.1415;
+    cout<<"Exit4"<<endl;
+    return (abs(dy1)<abs(dy2))?yaw1:yaw2;
 }
 
-//CHECK!!
-void camera_callback(const sensor_msgs::ImageConstPtr& msg)
+void insCallback(const tiburon::ins_data::ConstPtr& ypr)
 {
-    //http://wiki.ros.org/cv_bridge/Tutorials/UsingCvBridgeToConvertBetweenROSImagesAndOpenCVImages
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-
-    // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-
-    //http://wiki.ros.org/image_transport/Tutorials/SubscribingToImages
-    try
-    {
-        cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-        cv::waitKey(30);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-    }
-
+    currentYaw = ypr->YPR.x;
 }
 
-int main(int argc, char **argv)
+void segmentedImageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-    namedWindow("Orig",CV_WINDOW_NORMAL);
-    resizeWindow("Orig",432,324);
-    moveWindow("Orig",0,0);
-    namedWindow("Output",CV_WINDOW_NORMAL);
-    resizeWindow("Output",432,324);
-    moveWindow("Output",0,400);
-
-    ros::init(argc, argv, "bottom_camera_node");
-    ros::NodeHandle nh;
-    ros::Subscriber camera_sub = nh.subscribe("/auv_camera",1,camera_callback); //dooooo
-    image_transport::Subscriber yaw_sub = nh.subscribe<tiburon::ins_data>("/tiburon/ins_data",1,yaw_callback);
-
-    Mat orig,hsv,thresh;
-    orig = imread("/home/r2d2/savedim1.jpg");
-    if(orig.empty())
-    {
-        cout << "Image not read" << endl;
-        return 0;
-    }
-
-    ros::Rate loop_rate(10); //doooooooooooo
-
-    while(ros::ok())
-    {
-        Mat origdraw = orig.clone();
-        cvtColor(orig,hsv,CV_BGR2HSV);
-        Scalar colorlow = Scalar(14,122,93),colorhigh = Scalar(100,255,255);
-        inRange(hsv,colorlow,colorhigh,thresh);
-        bitwise_not(thresh,thresh);
-        //invert(thresh,thresh);
-        //Rect crop(100,100,540,380);
-        //orig = orig(crop);
-        vector<vector<Point> > contours;
-        findContours(thresh.clone(), contours, CV_RETR_CCOMP, CV_CHAIN_APPROX_NONE);
-        Mat newim = Mat::zeros(orig.size(),CV_8UC3);
-        //Mat edges; Canny(orig,edges,10,255); imshow("Orig",orig); imshow("Output",edges); waitKey(0); return 0;
-        int maxindex = 0;
-        if(contours.size()>0)
-        {
-            int maxarea = contourArea(contours[0]);
-
-            for(int i=1;i<contours.size();i++)
-            {
-                int area = contourArea(contours[i]);
-                if(maxarea < area)
-                {
-                    maxindex =i;
-                    maxarea = area;
-                }
-            }
-        }
-        else
-        {
-            cout << "No contour found " << endl;
-            return 0;
-        }
-        cout << "index " << maxindex << endl;
-        cout << contours.size() << endl;
-        //for(int i=0;i<contours.size();i++)
-        vector<vector<Point> > polys;
-        vector<Point> approxContour;
-        approxPolyDP(contours[maxindex],approxContour,10,true); // works wonders but this epsilon how much to set? - 10 works wonders
-        //Notes - Have to check whether returned polygon is of size 4
-        //        Which 2 sides to check for angles? - Length of side is not ok to check
-        //
-        polys.push_back(approxContour);
-        cout << "approx polygon\n" << approxContour << endl;
-        drawContours(newim,polys,0,Scalar(0,255,255));
-        drawContours(origdraw,polys,0,Scalar(0,255,255));
-
-        vector<double> angles;
-        double diffangle;
-        cout << "angles ";
-        for(int i= 0;i<approxContour.size();i++)
-        {
-            switch(i)
-            {
-                case 0: cv::putText(newim,"1",approxContour[i],cv::FONT_HERSHEY_COMPLEX_SMALL,1,cv::Scalar(255, 255, 255)); break;
-                case 1: cv::putText(newim,"2",approxContour[i],cv::FONT_HERSHEY_COMPLEX_SMALL,1,cv::Scalar(255, 255, 255)); break;
-                case 2: cv::putText(newim,"3",approxContour[i],cv::FONT_HERSHEY_COMPLEX_SMALL,1,cv::Scalar(255, 255, 255)); break;
-                case 3: cv::putText(newim,"4",approxContour[i],cv::FONT_HERSHEY_COMPLEX_SMALL,1,cv::Scalar(255, 255, 255)); break;
-            }
-            if(i==approxContour.size()-1)
-                diffangle = cc_angle(approxContour[i],approxContour[0]);
-            else
-                diffangle = cc_angle(approxContour[i],approxContour[i+1]);
-            //diffangle = atan2(sin(diffangle),cos(diffangle));
-            angles.push_back(diffangle*180/3.1415);
-            cout << angles[i] << "\t";
-        }
-        cout << endl;
-        cout << -angles[0]+angles[2] << endl;
-    
-        loop_rate.sleep();
-    }
-    
-
-    //cout << "angles\t" << angles << endl;
-
-// //This is not very nice ... le me no likes.
- //   drawContours(newim,contours,maxindex,Scalar::all(255));
- //   Rect bound = boundingRect(contours[maxindex]);
- //   cout << bound << endl;
-//    int count = 0;
-//    //double noiseDist = line.cols/80;
-//    vector<double> linecent;
-//    Point cent;
-
-//    for(int i = bound.y+1;i<bound.y+bound.height-1;i++)
+    //cout<<"seg"<<endl;
+   try
+   {
+     origGr = cv_bridge::toCvShare(msg, "mono8")->image;
+     //waitKey(100);
+   }
+   catch (cv_bridge::Exception& e)
+   {
+     ROS_ERROR("Could not convert from '%s' to 'mono8'.", msg->encoding.c_str());
+   }
+   segmentedImageReceived = true;
+}
+// void origImageCallback(const sensor_msgs::ImageConstPtr& msg)
+// {
+//     cout<<"orig"<<endl;
+//    try
 //    {
-//        for(int j=bound.x;j<bound.x+bound.width;j++)
-//        {
-//            if(newim.at<Vec3b>(Point(j,i))[0])
-//            {
-//                if(count==0)
-//                {
-//                    cent = Point(j,i);
-//                    count++;
-//                    continue;
-//                }
-//            //	/*else if(dst(cent,Point(j,i)) < noiseDist)
-//            //	{
-//            //		cent.x = (cent.x+j)/(cnt+1);
-//            //	}
-//                else
-//                {
-//                    cent.x = (cent.x*count+j)/(count+1);
-//                    count++;
-//                }
-//            }
-//        }
-//        circle(newim,cent,2,Scalar(0,0,255));
-//        //cout << cent << endl;  imshow("Orig",orig);  imshow("Output",newim);   waitKey(0);
-//        if(count>1)
-//        linecent.push_back(cent.x);
-//        cent = Point(0,0);
-//        count = 0;
+//      orig = cv_bridge::toCvShare(msg, "bgr8")->image;
+//      waitKey(100);
 //    }
-//    int grad = 0;
-//    for (int i = 0; i < (linecent.size() - 2); i++)
+//    catch (cv_bridge::Exception& e)
 //    {
-//        grad += (linecent[i + 1] - linecent[i]);
-//       // cout <<"grad = "<< grad << endl;
-//        //cout << linecent[i] << endl;
+//      ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
 //    }
-//    cout <<"grad = "<< grad << endl;
-    imshow("Orig",origdraw);
-    imshow("Output",newim);
-    waitKey(0);
-
-    ros::spin();
-
-    return 0;
-
-}
-
+//    origImageReceived = true;
+// }
---
+++ tiburon/src/segmentationNode.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <tiburon/hsv_data.h>
+
+using namespace cv;
+using namespace std;
+
+bool imageReceived = false;
+Mat inp, out;
+int hl=71,sl=38,vl=87,hh=138,sh=255,vh=255;
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+   try
+   {
+     inp = cv_bridge::toCvShare(msg, "bgr8")->image;
+     //imshow("Img",cv_bridge::toCvShare(msg, "bgr8")->image );
+   }
+   catch (cv_bridge::Exception& e)
+   {
+     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+   }
+   imageReceived=true;
+}
+
+void hsvCallback(const tiburon::hsv_data::ConstPtr& hsv)
+{
+    hl = hsv->hl;
+    sl = hsv->sl;
+    vl = hsv->vl;
+    hh = hsv->hh;
+    sh = hsv->sh;
+    vh = hsv->vh;
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "SegmentationNode");
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber imageSub = it.subscribe("auv_cam1",1,imageCallback);
+  image_transport::Publisher segPub = it.advertise("bottomCameraSegmented", 1);
+  ros::Subscriber hsvData = nh.subscribe("/tiburon/hsv_data",1,hsvCallback);
+  sensor_msgs::ImagePtr msg;
+  while(1)
+  {
+    if(imageReceived)
+    {
+      //imshow("Inp",inp);
+      cvtColor(inp,inp,CV_BGR2HSV);
+      inRange(inp,Scalar(hl,sl,vl),Scalar(hh,sh,vh),out);
+      //cout<<"here"<<out.channels()<<endl;
+      msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", out).toImageMsg();
+      segPub.publish(msg);
+      imageReceived = false;
+    }
+    if((char)waitKey(1)==27)
+    {
+      break;
+    }
+    ros::spinOnce();
+  }
+}
